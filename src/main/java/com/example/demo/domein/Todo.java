60910f84e57af41fa375eeccd9bffbeab7fc2691
SoyaMiyoshi <soya.miyoshi@gmail.com>
2019-08-03 20:00:48 +0900

    とりあえずCRUD成功

--- src/main/java/com/example/demo/domein/Todo.java
+++ src/main/java/com/example/demo/domein/Todo.java
@@ -10,7 +10,7 @@ public class Todo {
     @Id // ②
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ③
     private Long id;
-    private String body;
+    private String name;
 
     public Long getId() {
         return id;
@@ -18,16 +18,16 @@ public class Todo {
     public void setId(Long id) {
         this.id = id;
     }
-    public String getBody() {
-        return body;
+    public String getName() {
+        return name;
     }
-    public void setBody(String name) {
-        this.body = name;
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
     public String toString() {
-        return "Player [id=" + id + ", body=" + body +  "]";
+        return "Player [id=" + id + ", name=" + name +  "]";
     }
 }
 
