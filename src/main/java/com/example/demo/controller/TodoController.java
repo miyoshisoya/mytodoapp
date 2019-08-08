6b5f5d638c885fe31d6b53eed9026cc94dc1c4e4
SoyaMiyoshi <soya.miyoshi@gmail.com>
2019-08-08 11:34:10 +0900

    repoに関数追加できない

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,7 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.context.MessageSource;
+
+
 import com.example.demo.domein.Todo;
+import com.example.demo.repository.TodoRepository;
 import com.example.demo.service.TodoService;
 
 @Controller
@@ -20,6 +28,12 @@ import com.example.demo.service.TodoService;
 public class TodoController {
     @Autowired
     private TodoService todoService;
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @Autowired
+    MessageSource msg;
 
     @GetMapping
     public String index(Model model) { // ②
@@ -73,4 +87,40 @@ public class TodoController {
         todoService.delete(id);
         return "redirect:/todos";
     }
+
+    /*
+    
+    @GetMapping("search")
+    public String search(Model model, @RequestParam(defaultValue = "") String name) {
+        List<Todo> tasks = todoService.findTodos(name);
+        model.addAttribute("taskNumSearched", -1);
+        if (name.equals("")) {
+            return "search";
+        }
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("taskNumSearched", tasks.size());
+        return "search";
+    }*/
+
+
+    /*
+    @PostMapping("todos/search")
+    public ModelAndView search(@RequestParam String keyword) {
+    System.out.println(keyword);
+    
+    ModelAndView mv = new ModelAndView();
+    
+    mv.setViewName("/todos");
+    if (keyword.length() > 0) {
+        System.out.println(keyword);
+        List<Todo> list = todoRepository.findTodos(keyword);
+        if (CollectionUtils.isEmpty(list)) {
+          String message = msg.getMessage("not found!", null,  Locale.JAPAN);
+          mv.addObject("emptyMessage", message);
+        }
+        mv.addObject("list", list);
+      }
+      return mv;
+    }*/
+   
 }
--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.repository;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import com.example.demo.domein.Todo;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
+public interface TodoRepository extends JpaRepository<Todo, Long> {   
 
+    List<Todo> findTodos(String name);
+    //String findTodos(String name);
+
+}
